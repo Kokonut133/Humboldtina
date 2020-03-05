2dc41e38bea4de2084693a53df2536562bce37fd
Kokonut133 <christianstur@gmail.com>
2020-03-05 17:35:48 +0100

    starting automating dataset setup

--- init_structure.py
+++ init_structure.py
@@ -1,7 +1,7 @@
 import os
-import subprocess
 import settings
-
+import subprocess
+import zipfile
 
 if __name__ == '__main__':
     # inits resource file structure
@@ -10,7 +10,13 @@ if __name__ == '__main__':
     os.makedirs(settings.dataset_dir, exist_ok=True)
     os.makedirs(settings.result_dir, exist_ok=True)
 
-    # downloads initial given dataset
-    bashCommand = "kaggle competitions download -c ds4g-environmental-insights-explorer -p "+str(settings.dataset_dir)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+    # downloads initial given dataset and unzips
+    ds4g_dir = os.path.join(settings.dataset_dir, "ds4g-environmental-insights-explorer")
+    if not os.path.exists(ds4g_dir):
+        bashCommand = "kaggle competitions download -c ds4g-environmental-insights-explorer -p "+str(settings.dataset_dir)
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+        with zipfile.ZipFile(ds4g_dir+".zip", 'r') as zip_ref:
+            zip_ref.extractall(ds4g_dir)
+        os.remove(ds4g_dir+".zip")
