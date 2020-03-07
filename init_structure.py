63055759bfec58b532e18e0b082b210bc02e517a
Kokonut133 <christianstur@gmail.com>
2020-03-07 14:59:25 +0100

    folium

--- init_structure.py
+++ init_structure.py
@@ -9,11 +9,13 @@ if __name__ == '__main__':
     os.makedirs(settings.img_dir, exist_ok=True)
     os.makedirs(settings.dataset_dir, exist_ok=True)
     os.makedirs(settings.result_dir, exist_ok=True)
+    os.makedirs(settings.random_dir, exist_ok=True)
+
 
     # downloads initial given dataset and unzips
     ds4g_dir = os.path.join(settings.dataset_dir, "ds4g-environmental-insights-explorer")
     if not os.path.exists(ds4g_dir):
-        bashCommand = "kaggle competitions download -c ds4g-environmental-insights-explorer -p "+str(settings.dataset_dir)
+        bashCommand = "kaggle competitions download -c ds4g-environmental-insights-explorer -p " + str(settings.dataset_dir)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
